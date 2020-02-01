b56b34579578b054f9e1c4cdbab65422483eccea
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-02-01 19:40:38 +0000

    cypress setup

--- cypress/integration/first-test.spec.js
+++ cypress/integration/first-test.spec.js
@@ -1,5 +1,15 @@
+// import {add} from '../../src/service';
+import App from '../../src/App'
 describe("First test", () => {
     it("will pass", () => {
         expect(true).to.equal(true);
-    })
+    });
+
+    it("App will be defined", () => {
+        expect(1).to.exist
+    });
+
+    // it("will correctly add numbers", () => {
+    //     expect(add(2,3)).to.equal(5);
+    // })
 });
