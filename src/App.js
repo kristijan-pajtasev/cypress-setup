46d2600f86ad9c80ba2df1d12d7e488121027881
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-02-01 19:39:57 +0000

    service included

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {add} from './service';
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React{add(2,3)}
         </a>
       </header>
     </div>
